55c4be52832b0146d4e00f379ac6e203da6a8bfe
Paul Milliken <paul.milliken@scionresearch.com>
2013-08-14 08:30:11 +1200

    Added cfg file to prepare for dynamic reconfigure, created setup.py, modified CMakeLists.txt and copied test_speed_control.launch to tests

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+#fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages = ['axis_teleop'],
+    package_dir = {'':'src'}
+    requires = ['std_msgs', 'rospy', 'axis_camera']
+)
+
+setup(**setup_args)
